1b2411777ad6f0d27793a58a6fc7eda302501acc
vanpana <van.panaite@gmail.com>
2018-05-05 16:09:24 +0300

    created double items

--- Templates/Drawer.php
+++ Templates/Drawer.php
@@ -2,6 +2,7 @@
 
 class Drawer {
     public static function getIconFilledTemplate($imgurl, $title, $price, $category, $description) {
+        // Get the template
         $template = file_get_contents("Templates/item/item_template.html");
 
         // Place image
@@ -11,13 +12,28 @@ class Drawer {
         $template = str_replace("{{title}}", $title, $template);
 
         // Place price
-        $template = str_replace("{{price}}", $price, $template);
+        $template = str_replace("{{price}}", '$' . $price, $template);
 
         // Place category
         $template = str_replace("{{category}}", $category, $template);
 
         // Place description
         $template = str_replace("{{description}}", $description, $template);
+
+        return $template;
+    }
+
+    public static function getDoubleItemsFilledTemplate($item1, $item2) {
+        // Get the template
+        $template = file_get_contents("Templates/item/double_items_template.html");
+
+        // Place first item
+        $template = str_replace("{{item1}}", self::getIconFilledTemplate($item1->image, $item1->name,
+            $item1->price, $item1->category, $item1->description), $template);
+
+        // Place second item
+        $template = str_replace("{{item2}}", self::getIconFilledTemplate($item2->image, $item2->name,
+            $item2->price, $item2->category, $item2->description), $template);
 
         return $template;
     }
--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="Templates/item/item.css">
+    <link rel="stylesheet" type="text/css" href="Templates/item/double_items.css">
 </head>
 
 <body>
-<p>Hello world!</p>
-
 <?php
 require("Util/Database.php");
 require("Repository/Repository.php");
 require("Domain/Product.php");
 require("Templates/Drawer.php");
 
-foreach (Repository::getAllProducts() as $product) {
-    echo Drawer::getIconFilledTemplate($product->image, $product->name, $product->price, $product->category,
-        $product->description);
+$products = Repository::getAllProducts();
+
+for ($index = 0; $index < count($products) - 1; $index = $index + 2) {
+    $product1 = $products[$index];
+    $product2 = $products[$index + 1];
+    echo Drawer::getDoubleItemsFilledTemplate($product1, $product2);
 }
+
+
+//foreach (Repository::getAllProducts() as $product) {
+//    echo Drawer::getIconFilledTemplate($product->image, $product->name, $product->price, $product->category,
+//        $product->description);
+//    echo "";
+//}
 ?>
 </body>
 </html>
