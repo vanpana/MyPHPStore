2795cb9404d43fbeb00686a0894b11ae2c45f4c2
vanpana <van.panaite@gmail.com>
2018-05-06 14:02:42 +0300

    added topbar

--- client/index.php
+++ client/index.php
@@ -5,6 +5,10 @@
     <title>My shop</title>
 
     <!--    css-->
+    <!--    index css-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <!--    product css-->
     <link rel="stylesheet" type="text/css" href="Templates/item/item.css">
     <link rel="stylesheet" type="text/css" href="Templates/item/double_items.css">
 
@@ -13,6 +17,13 @@
     <script src="Requests/getProducts.js"></script>
 </head>
 <body>
+<div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+</div>
+
 <div id="products"></div>
 </body>
 </html>
