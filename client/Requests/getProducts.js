287cffb4fa3ea33df9394a8e341915d8a4b16803
vanpana <van.panaite@gmail.com>
2018-05-06 14:24:35 +0300

    added cookies

--- client/Requests/getProducts.js
+++ client/Requests/getProducts.js
@@ -25,7 +25,18 @@ function drawProductsByIndex(startIndex) {
 }
 
 function addToCart(index) {
-    alert("Bought product with id: " + index);
+    let cartCookieJSON = getCookie("cart");
+    console.log(cartCookieJSON);
+    var cartList;
+
+    if (cartCookieJSON === "") {
+        cartList = [];
+        cartList.push(index);
+    } else {
+        cartList = JSON.parse(cartCookieJSON);
+        cartList.push(index);
+    }
+    setCookie("cart", JSON.stringify(cartList), 30);
 }
 
 function changeActive(tab) {
@@ -33,4 +44,27 @@ function changeActive(tab) {
     document.getElementById("cartbtn").classList.remove('active');
 
     document.getElementById(tab).classList.add('active');
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
